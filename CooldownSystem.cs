764086f22d65740cb1ce51235fc5e1f55b74fdae
Charrdge <ivan.527413@gmail.com>
2022-07-17 16:38:55 +0300

    Выделена логика расчёта откатов в отдельную сущность. Добавлена возможность указания собственных клавиш переключения персонажей. Добавлена возможность указания максимального расчетного времени отката.

---
+++ CooldownSystem.cs
@@ -0,0 +1,92 @@
+﻿using System.Windows.Forms;
+
+internal class CooldownSystem : IDisposable
+{
+	/// <summary>
+	/// Хранит время с последнего нажатия навыка каждого персонажа
+	/// </summary>
+	private Dictionary<Keys, DateTime> LastClickTimes = new();
+
+	private Keys[] SelectKeys { get; set; }
+
+	/// <summary>
+	/// Последняя нажатая клавиша переключения персонажа
+	/// </summary>
+	private Keys LastSelectCharKey { get; set; }
+
+	public TimeSpan? MaxCooldownTime { get; }
+
+	public CooldownSystem(GraphicsWorker graphics, TimeSpan? maxCooldownTime = null) 
+		: this(graphics, maxCooldownTime, new Keys[] { Keys.D1, Keys.D2, Keys.D3, Keys.D4 }) { }
+
+	public CooldownSystem(GraphicsWorker graphics, TimeSpan? maxCooldownTime, Keys[] switchChararcterKeys)
+	{
+		LastSelectCharKey = switchChararcterKeys[0];
+
+		SelectKeys = switchChararcterKeys;
+		foreach (Keys key in switchChararcterKeys) LastClickTimes.Add(key, DateTime.Now);
+
+		ButtonHook.OnKeyDown += (vkCode) =>
+		{
+			var key = (Keys)vkCode;
+
+			if (LastClickTimes.ContainsKey(key)) LastSelectCharKey = key;
+			else if (key == Keys.E) LastClickTimes[LastSelectCharKey] = DateTime.Now;
+		};
+
+		graphics.OnDrawGraphics += (sender, e) =>
+		{
+			var text = (Keys key) =>
+			{
+				TimeSpan span = DateTime.Now - LastClickTimes[key];
+				if (span >= MaxCooldownTime) return $"00:00:00";
+				return $"{span.Minutes}:{span.Seconds}:{span.Milliseconds / 10}";
+			};
+
+			int v = 1300; // расположение по горизонтали
+			int h = 225; // расположение по вертикали
+			int p = 55; // коэффициент смещения
+
+			for (int i = 0; i < SelectKeys.Length; i++)
+			{
+				var key = SelectKeys[i];
+
+				e.DrawTextWithBackground(graphics.Fonts["consolas"], graphics.Brushes["white"], graphics.Brushes["black"], v, h + (p * i), text(key));
+			}
+		};
+
+		MaxCooldownTime = maxCooldownTime;
+	}
+	
+	~CooldownSystem() => Dispose(disposing: false);
+
+	#region IDisposable
+	private bool _disposedValue;
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!_disposedValue)
+		{
+			if (disposing)
+			{
+				// TODO: освободить управляемое состояние (управляемые объекты)
+			}
+
+
+
+			// TODO: освободить неуправляемые ресурсы (неуправляемые объекты) и переопределить метод завершения
+			// TODO: установить значение NULL для больших полей
+			_disposedValue = true;
+		}
+	}
+
+	// // TODO: переопределить метод завершения, только если "Dispose(bool disposing)" содержит код для освобождения неуправляемых ресурсов
+
+
+	void IDisposable.Dispose()
+	{
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+	#endregion
+}
---
+++ GraphicsWorker.cs
@@ -0,0 +1,113 @@
+﻿using GameOverlay.Drawing;
+using GameOverlay.Windows;
+using System.Windows.Forms;
+
+internal class GraphicsWorker : IDisposable
+{
+	/// <summary>
+	/// Объект через который происходит генерация графики
+	/// </summary>
+	private StickyWindow Overlay { get; init; }
+
+	/// <summary>
+	/// Словарь цветов
+	/// </summary>
+	public Dictionary<string, SolidBrush> Brushes { get; } = new();
+	
+	/// <summary>
+	/// Словарь шрифтов
+	/// </summary>
+	public Dictionary<string, Font> Fonts { get; } = new();
+
+	/// <summary>
+	/// Создаёт новый экземпляр обработчика графики
+	/// </summary>
+	/// <param name="processhandle"></param>
+	public GraphicsWorker(IntPtr processhandle)
+	{
+		Graphics gfx = new()
+		{
+			MeasureFPS = true,
+			PerPrimitiveAntiAliasing = true,
+			TextAntiAliasing = true
+		};
+
+		Overlay = new(processhandle, gfx)
+		{
+			FPS = 15,
+			IsTopmost = true,
+			IsVisible = true
+		};
+
+		Overlay.SetupGraphics += Overlay_SetupGraphics;
+		Overlay.DrawGraphics += Overlay_DrawGraphics;
+		Overlay.DestroyGraphics += Overlay_DestroyGraphics;
+	}
+
+	~GraphicsWorker() => Dispose(false);
+
+	public void Run()
+	{
+		Overlay.Create();
+		Overlay.Join();
+	}
+
+	public delegate void DrawGraphic(object? sender, Graphics graphics);
+
+	public event DrawGraphic? OnDrawGraphics;
+
+	#region Graphic event methods
+	private void Overlay_SetupGraphics(object? sender, SetupGraphicsEventArgs e)
+	{
+		var gfx = e.Graphics;
+
+		if (e.RecreateResources)
+		{
+			foreach (var pair in Brushes) pair.Value.Dispose();
+			foreach (var pair in Fonts) pair.Value.Dispose();
+		}
+
+		Brushes["black"] = gfx.CreateSolidBrush(0, 0, 0);
+		Brushes["white"] = gfx.CreateSolidBrush(255, 255, 255);
+
+		Fonts["consolas"] = gfx.CreateFont("Consolas", 14);
+
+		if (e.RecreateResources) return;
+	}
+
+	private void Overlay_DrawGraphics(object? sender, DrawGraphicsEventArgs e)
+	{
+		var gfx = e.Graphics;
+
+		gfx.ClearScene();
+
+		OnDrawGraphics?.Invoke(this, gfx);
+	}
+
+	private void Overlay_DestroyGraphics(object? sender, DestroyGraphicsEventArgs e)
+	{
+		foreach (var pair in Brushes) pair.Value.Dispose();
+		foreach (var pair in Fonts) pair.Value.Dispose();
+	}
+	#endregion
+
+	#region IDisposable
+	private bool _disposedValue;
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!_disposedValue)
+		{
+			Overlay.Dispose();
+
+			_disposedValue = true;
+		}
+	}
+
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+	#endregion
+}
