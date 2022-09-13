6bd2844b719ef17ce4adf92f390a80426effac40
Charrdge <ivan.527413@gmail.com>
2022-09-13 16:33:11 +0300

    Добавлены новые типы для удобной работы с графическими ресурсами

---
+++ GraphicWorkers/FontWorker.cs
@@ -0,0 +1,34 @@
+﻿using GameOverlay.Drawing;
+
+namespace GenshinImpactOverlay.GraphicWorkers
+{
+	public class FontWorker
+	{
+		public string FontFamilyName { get; }
+
+		public float Size { get; }
+
+		public bool Bold { get; }
+
+		public bool Italic { get; }
+
+		public bool WordWrapping { get; }
+
+		private Font? Font { get; set; }
+
+		public FontWorker(string fontFamilyName, float size, bool bold = false, bool italic = false, bool wordWrapping = false)
+		{
+			FontFamilyName = fontFamilyName;
+			Size = size;
+			Bold = bold;
+			Italic = italic;
+			WordWrapping = wordWrapping;
+		}
+
+		public void Create(Graphics gfx) => Font = gfx.CreateFont(FontFamilyName, Size, Bold, Italic, WordWrapping);
+
+		public void Dispose() => Font?.Dispose();
+
+		public static implicit operator Font(FontWorker fontWorker) => fontWorker.Font ?? throw new NullReferenceException($"{nameof(Font)} not initialized");
+	}
+}
---
+++ GraphicWorkers/SolidBrushWorker.cs
@@ -0,0 +1,19 @@
+﻿using GameOverlay.Drawing;
+
+namespace GenshinImpactOverlay.GraphicWorkers
+{
+	public class SolidBrushWorker
+	{
+		public Color Color { get; }
+
+		private SolidBrush? Brush { get; set; }
+
+		public SolidBrushWorker(Color color) => Color = color;
+
+		public void Create(Graphics gfx) => Brush = gfx.CreateSolidBrush(Color);
+
+		public void Dispose() => Brush?.Dispose();
+
+		public static implicit operator SolidBrush(SolidBrushWorker worker) => worker.Brush ?? throw new NullReferenceException($"{nameof(Brush)} not initialized");
+	}
+}
