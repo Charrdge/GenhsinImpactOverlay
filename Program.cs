29d17c00a68703bc4e72e593d5143e5e35ca15cf
Charrdge <ivan.527413@gmail.com>
2022-09-27 18:40:08 +0300

    При запуске сборки DEBUG - оверлей запускается поверх студии для упрощённой проверки без нагрузки игрой

--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Windows.Forms;
 
+#if DEBUG
+Console.WriteLine("Hello, Debug world!");
+string name = "devenv";
+#else
+Console.WriteLine("Hello, world!");
 string name = "GenshinImpact";
+#endif
+
 IntPtr handle = System.Diagnostics.Process.GetProcessesByName(name).First().MainWindowHandle;
 
 GameOverlay.TimerService.EnableHighPrecisionTimers();
 
-Task hook = new Task(ButtonHook.Run);
+Task hook = new(ButtonHook.Run);
 hook.Start();
 
-using (var example = new GraphicWorker(handle))
+using (GraphicsWorker graphicsWorker = new(handle))
 {
-	example.Run();
+	graphicsWorker.AddFont("consolas", "Consolas", 14);
+
+	graphicsWorker.AddSolidBrush("black", new GameOverlay.Drawing.Color(0, 0, 0));
+	graphicsWorker.AddSolidBrush("white", new GameOverlay.Drawing.Color(255, 255, 255));
+
+	var cooldownSystem = new CooldownSystem(graphicsWorker, new Keys[] { Keys.D1, Keys.D2, Keys.D3, Keys.D4 }, new TimeSpan(0, 1, 30));
+	graphicsWorker.Run();
 }
