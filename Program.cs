ab3ee06d2af9312071a8796c8794a94ad82508bd
Charrdge <ivan.527413@gmail.com>
2022-11-21 20:53:34 +0300

    Добавлен перехват ввода в приложение через смену основного окна

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
+#region Hello
 #if DEBUG
 Console.WriteLine("Hello, Debug world!");
 string name = "devenv";
@@ -7,20 +9,48 @@ string name = "devenv";
 Console.WriteLine("Hello, world!");
 string name = "GenshinImpact";
 #endif
+#endregion
 
-IntPtr handle = System.Diagnostics.Process.GetProcessesByName(name).First().MainWindowHandle;
-
-GameOverlay.TimerService.EnableHighPrecisionTimers();
-
+#region Button hook
 Task hook = new(ButtonHook.Run);
 hook.Start();
+#endregion
 
-using (GraphicsWorker graphicsWorker = new(handle))
+IntPtr handleWindow = System.Diagnostics.Process.GetProcessesByName(name).First().MainWindowHandle;
+IntPtr overlayWindow = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+
+#region Foreground switching
+[DllImport("user32.dll")]
+static extern bool AllowSetForegroundWindow(int dwProcessId);
+
+[DllImport("user32.dll")]
+static extern bool SetForegroundWindow(IntPtr hWnd);
+
+bool isAllowedSetForegroundWindow = AllowSetForegroundWindow(System.Diagnostics.Process.GetCurrentProcess().Id);
+
+#if DEBUG
+Console.WriteLine(isAllowedSetForegroundWindow.ToString());
+#endif
+
+ButtonHook.OnKeyDown += (int vkCode) =>
+{
+	if ((Keys)vkCode == Keys.C)
+	{
+		bool foregroundWindowHasSet = SetForegroundWindow(overlayWindow);
+#if DEBUG
+		Console.WriteLine($"Set foreground window - {foregroundWindowHasSet}");
+#endif
+	}
+};
+#endregion
+
+GameOverlay.TimerService.EnableHighPrecisionTimers();
+using (GraphicsWorker graphicsWorker = new(handleWindow))
 {
 	var keys = new Keys[] { Keys.D1, Keys.D2, Keys.D3, Keys.D4 };
-	var cooldownSystem = new CooldownSystem(graphicsWorker, keys, new TimeSpan(0, 1, 30));
+	var cooldownSystem = new GenshinImpactOverlay.Cooldowns.CooldownSystem(graphicsWorker, keys, new TimeSpan(0, 1, 30));
 
-
+	var chanSystem = new GenshinImpactOverlay.ImageBoard.ImageBoardSystem(graphicsWorker);
 
 	graphicsWorker.Run();
 }
