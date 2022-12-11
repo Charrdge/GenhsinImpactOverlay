26974e76f9b0803117b759e00a73c2d9305c8731
Charrdge <ivan.527413@gmail.com>
2022-12-11 04:00:54 +0300

    Обновлена система воспроизведения музыки. Теперь класс MusicSystem является абстрактным и содержит функционал общего для различных музыкальных провайдеров управления воспроизведением.  Создан интерфейс IMusicData для хранения данных конкретных треков.

---
+++ Music/ITrackData.cs
@@ -0,0 +1,14 @@
+﻿namespace GenshinImpactOverlay.Music;
+
+internal interface ITrackData
+{
+	/// <summary>
+	/// Название трека
+	/// </summary>
+	public string TrackName { get; }
+
+	/// <summary>
+	/// Ссылка на трек
+	/// </summary>
+	public string Link { get; }
+}
--- Music/MusicSystem.cs
+++ Music/MusicSystem.cs
@@ -1,29 +1,32 @@
-﻿using System.Text.Json;
-using Newtonsoft.Json.Linq;
-using YandexMusicApi;
-using NAudio.Wave;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using GameOverlay.Drawing;
-using System.Text.Json.Nodes;
 using GenshinImpactOverlay.EventsArgs;
-using static GraphicsWorker;
 using GenshinImpactOverlay.Menus;
+using NAudio.Wave;
 
 namespace GenshinImpactOverlay.Music;
 
-internal class MusicSystem : IUseMenu
+internal abstract class MusicSystem : IUseMenu
 {
 	public const string SYSNAME = "Music";
 
-	private GraphicsWorker Worker { get; init; }
+	#region Fonts
+	private string FontIndex { get; init; }
+	private string BlackBrushIndex { get; init; }
+	private string WhiteBrushIndex { get; init; }
+	#endregion
 
-	#region Player fields
+	#region Player
+	private MediaFoundationReader? UrlMusicReader { get; set; }
 	private WasapiOut? Player { get; set; }
-	private MediaFoundationReader? Mf { get; set; }
-	#endregion Player fields
+	#endregion Player
 
-	private string? SoundName { get; set; }
-	private string? PlayTrackId { get; set; }
+	#region Track list
+	private LinkedList<ITrackData> Tracks { get; } = new();
+	protected ITrackData? NowPlaying { get; private set; }
+	#endregion Track list
+
+	#region Volume
 	private decimal _volume = 0.5m;
 	private decimal Volume
 	{
@@ -34,199 +37,39 @@ internal class MusicSystem : IUseMenu
 			UpdatePlayerVolume(_volume);
 		}
 	}
+	#endregion Volume
 
-	#region Resources
-	private string FontIndex { get; init; }
-	private string WhiteBrushIndex { get; init; }
-	private string BlackBrushIndex { get; init; }
-	#endregion Resources
+	private GraphicsWorker Worker { get; init; }
 
-	#region Station data
-	private string StationId { get; set; } = "user:onyourwave";
-	private Dictionary<string, JToken> StationTracks { get; } = new();
-	#endregion Station data
-	
-	public MusicSystem(GraphicsWorker worker)
+	public MusicSystem(GraphicsWorker worker, Action<string> updateLoadStatus)
 	{
+		updateLoadStatus("Connect to graphic");
 		Worker = worker;
-		
+
+		updateLoadStatus("Load graphic resources");
 		FontIndex = worker.AddFont("Consolas", 14);
 		WhiteBrushIndex = worker.AddSolidBrush(new Color(255, 255, 255));
 		BlackBrushIndex = worker.AddSolidBrush(new Color(0, 0, 0));
 
-		Autorize();
+		updateLoadStatus("Autorize");
+		Autorize(GetLogin, GetPassword);
 
-		Console.WriteLine(Token.token);
-		
-		//var stateDash = Rotor.StationDashboard();
-		//var station = stateDash["result"]["stations"].First;
-
-		PlayNextStationTrack();
+		updateLoadStatus("Start playing queue");
+		PlayNexTrack();
 
 		InputHook.OnKeyUp += ButtonHook_OnKeyDown;
 
 		worker.OnDrawGraphics += Worker_OnDrawGraphics;
-	}
 
-	private void Worker_OnDrawGraphics(object? sender, OnDrawGraphicEventArgs e)
-	{
-		if (SoundName is not null)
-		{
-			Point point = new(15, 15);
-			if (Worker.Fonts[FontIndex].IsInitialized && Worker.Brushes[WhiteBrushIndex].IsInitialized)
-				e.Graphics.DrawText(Worker.Fonts[FontIndex], Worker.Brushes[WhiteBrushIndex], point, SoundName);
-		}
-	}
-
-	private void ButtonHook_OnKeyDown(object? _, OnKeyUpEventArgs eventArgs)
-	{
-		if (eventArgs.InputPriority > InputPriorityEnum.Normal && eventArgs.System != SYSNAME) return;
-	}
-	
-	private void SwitchTrackPause()
-	{
-		if (Player is not null) switch (Player.PlaybackState)
-		{
-			case PlaybackState.Paused:
-				Player.Play();
-				break;
-			case PlaybackState.Playing:
-				Player.Pause();
-				break;
-		}
-	}
-
-	private void PlayNextStationTrack()
-	{
-		if (!StationTracks.Any()) UpdateStationTracksQueue(StationId);
-		if (PlayTrackId is null) PlayTrackId = StationTracks.First().Key;
-
-		var unplayedTracks = StationTracks.SkipWhile((pair) => pair.Key != PlayTrackId).Skip(1);
-
-		if (unplayedTracks.Count() < 2) UpdateStationTracksQueue(StationId); 
-
-		var track = unplayedTracks.Any() ? unplayedTracks.First() : StationTracks.First();
-
-		string link = GetTrackLink(track.Key);
-
-		PlayTrackId = track.Key;
-
-		SoundName = $"{track.Value["artists"].First["name"].Value<string>()} - {track.Value["title"].Value<string>()}";
-
-		PlayTrack(link);
-	}
-
-	private void PlayPrevStationTrack()
-	{
-		if (StationTracks.Any())
-		{
-			var prevTracks = StationTracks.Reverse().SkipWhile((pair) => pair.Key != PlayTrackId).Skip(1);
-
-			if (prevTracks.Any())
-			{
-				var track = prevTracks.First();
-
-				string link = GetTrackLink(track.Key);
-
-				PlayTrackId = track.Key;
-
-				SoundName = $"{track.Value["artists"].First["name"].Value<string>()} - {track.Value["title"].Value<string>()}";
-
-				PlayTrack(link);
-			}
-		}
-	}
-
-	private void PlayTrack(string link)
-	{
-		if (Mf is not null) Mf.Dispose();
-		Mf = new(link);
-
-		if (Player is not null)
-		{
-			Player.Pause();
-		}
-
-		Player = new();
-		Player.PlaybackStopped += (sender, e) =>
-		{
-			if (sender is not null) ((WasapiOut)sender).Pause();
-			PlayNextStationTrack();
-		};
-		Player.Init(Mf);
-		Player.Play();
-		UpdatePlayerVolume(Volume);
-	}
-
-	private void UpdateStationTracksQueue(string stationId)
-	{
-		int count = StationTracks.Count;
-
-		do
-		{
-			var tracks = Rotor.GetTrack(stationId)["result"]["sequence"];
-
-			foreach (var item in tracks)
-			{
-				string id = item["track"]["id"].Value<string>();
-
-				if (!StationTracks.ContainsKey(id)) StationTracks.Add(id, item["track"]);
-			}
-
-			Thread.Sleep(100); // Ограничивает скорость отправки запросов во избежание фризов
-
-		} while (StationTracks.Count < count + 2);
-		//if (StationTracks.Count > 10) StationTracks = new(StationTracks.Skip(5));
-	}
-
-	private static string GetTrackLink(string trackId)
-	{
-		var info = Track.GetDownloadInfoWithToken(trackId);
-		var link = Track.GetDirectLink(info["result"].First["downloadInfoUrl"].Value<string>());
-
-		return link;
-	}
-
-	private string Autorize()
-	{
-		string fileName = "config.json";
-		string yaMusicJsonName = "yandex_music";
-		string tokenJsonName = "token";
-
-		JsonElement rootElement = JsonDocument.Parse(GetJsonFileAsString(fileName)).RootElement;
-		if (rootElement.TryGetProperty(yaMusicJsonName, out JsonElement yaMusicJson) && yaMusicJson.TryGetProperty(tokenJsonName, out JsonElement tokenJson))
-		{
-			Token.token = tokenJson.GetString();
-		}
-
-		while (Token.token is null || Token.token == "" || Account.ShowInformAccount()["error"] is not null)
-		{
-			(string login, string password) = GetPassData();
-			Token.GetToken(login, password);
-		}
-
-		JsonNode rootNode = JsonNode.Parse(GetJsonFileAsString(fileName)) ?? throw new NullReferenceException();
-
-		if (rootNode[yaMusicJsonName] is not null) rootNode[yaMusicJsonName][tokenJsonName] = Token.token;
-		else rootNode[yaMusicJsonName] = new JsonObject 
-		{
-			[tokenJsonName] = Token.token
-		};
-		using StreamWriter streamWriter = new(fileName);
-		streamWriter.Write(rootNode.ToString());
-
-		return Token.token;
-
-		(string login, string password) GetPassData()
+		string GetLogin()
 		{
 			string? login = "";
+
 			do
 			{
-				DrawGraphic onDrawGraphic_LoginEvent = (object? sender, OnDrawGraphicEventArgs e) =>
+				GraphicsWorker.DrawGraphic onDrawGraphic_LoginEvent = (object? sender, OnDrawGraphicEventArgs e) =>
 				{
-					if (Worker.Fonts[FontIndex].IsInitialized &&
-						Worker.Brushes[WhiteBrushIndex].IsInitialized &&
-						Worker.Brushes[BlackBrushIndex].IsInitialized)
+					if (Worker.Fonts[FontIndex].IsInitialized && Worker.Brushes[WhiteBrushIndex].IsInitialized && Worker.Brushes[BlackBrushIndex].IsInitialized)
 					{
 						e.Graphics.DrawTextWithBackground(
 							Worker.Fonts[FontIndex], Worker.Brushes[WhiteBrushIndex], Worker.Brushes[BlackBrushIndex],
@@ -237,21 +80,23 @@ internal class MusicSystem : IUseMenu
 				};
 
 				Worker.OnDrawGraphics += onDrawGraphic_LoginEvent;
-
 				login = InputHook.TryInputText((string loginProccess) => { login = loginProccess; return false; });
 
 				Worker.OnDrawGraphics -= onDrawGraphic_LoginEvent;
-
 			} while (login is null || login.Length == 0);
 
+			return login;
+		}
+
+		string GetPassword()
+		{
 			string? password = "";
+
 			do
 			{
-				DrawGraphic onDrawGraphic_LoginEvent = (object? sender, OnDrawGraphicEventArgs e) =>
+				GraphicsWorker.DrawGraphic onDrawGraphic_LoginEvent = (object? sender, OnDrawGraphicEventArgs e) =>
 				{
-					if (Worker.Fonts[FontIndex].IsInitialized && 
-						Worker.Brushes[WhiteBrushIndex].IsInitialized && 
-						Worker.Brushes[BlackBrushIndex].IsInitialized)
+					if (Worker.Fonts[FontIndex].IsInitialized && Worker.Brushes[WhiteBrushIndex].IsInitialized && Worker.Brushes[BlackBrushIndex].IsInitialized)
 					{
 						e.Graphics.DrawTextWithBackground(
 							Worker.Fonts[FontIndex], Worker.Brushes[WhiteBrushIndex], Worker.Brushes[BlackBrushIndex],
@@ -269,15 +114,63 @@ internal class MusicSystem : IUseMenu
 
 			} while (password is null || password.Length == 0);
 
-			return (login, password);
+			return password;
 		}
-	
-		static string GetJsonFileAsString(string fileName)
+	}
+
+	private void Worker_OnDrawGraphics(object? sender, OnDrawGraphicEventArgs e)
+	{
+		if (NowPlaying is not null)
 		{
-			using StreamReader reader = new(fileName);
-			string file = reader.ReadToEnd();
-			return file;
+			Point point = new(15, 15);
+			if (Worker.Fonts[FontIndex].IsInitialized && Worker.Brushes[WhiteBrushIndex].IsInitialized)
+				e.Graphics.DrawText(Worker.Fonts[FontIndex], Worker.Brushes[WhiteBrushIndex], point, NowPlaying.TrackName);
 		}
+	}
+
+	private void ButtonHook_OnKeyDown(object? _, OnKeyUpEventArgs eventArgs)
+	{
+		if (eventArgs.InputPriority > InputPriorityEnum.Normal && eventArgs.System != SYSNAME) return;
+	}
+
+	#region Player control methods
+	private void PlayNexTrack()
+	{
+		ITrackData next;
+		
+		if (Tracks.First is null) Tracks.AddFirst(GetNextTrack(Array.Empty<ITrackData>()));
+		
+		if (NowPlaying is null) next = Tracks.First.Value;
+		else
+		{
+			LinkedListNode<ITrackData> item = Tracks.FindLast(NowPlaying) ?? throw new NullReferenceException();
+			if (item.Next is null) Tracks.AddLast(GetNextTrack(Tracks.ToArray()));
+			next = item.Next.Value;
+		}
+
+		PlayTrack(next);
+	}
+
+	private void PlayPrevTrack()
+	{
+		if (Tracks.Any() && NowPlaying is not null)
+		{
+			LinkedListNode<ITrackData>? prev = Tracks.Find(NowPlaying)?.Previous;
+			if (prev is not null) PlayTrack(prev.Value);
+		}
+	}
+
+	private void SwitchTrackPause()
+	{
+		if (Player is not null) switch (Player.PlaybackState)
+			{
+				case PlaybackState.Paused:
+					Player.Play();
+					break;
+				case PlaybackState.Playing:
+					Player.Pause();
+					break;
+			}
 	}
 
 	private void UpdatePlayerVolume(decimal vol)
@@ -290,6 +183,40 @@ internal class MusicSystem : IUseMenu
 			}
 		}
 	}
+	#endregion Player control methods
+
+	private void PlayTrack(ITrackData track)
+	{
+		NowPlaying = track;
+		if (UrlMusicReader is not null) UrlMusicReader.Close();
+		UrlMusicReader = new(track.Link);
+
+		Player?.Pause();
+
+		Player = new();
+		Player.PlaybackStopped += (sender, e) =>
+		{
+			if (sender is not null) ((WasapiOut)sender).Pause();
+			PlayNexTrack();
+		};
+		Player.Init(UrlMusicReader);
+		Player.Play();
+		UpdatePlayerVolume(Volume);
+	}
+
+	/// <summary>
+	/// Инициирует авторизацию в системе
+	/// </summary>
+	/// <param name="getLogin">Метод для получения логина от пользователя</param>
+	/// <param name="getPassword">Метод для получения пароля от пользователя</param>
+	protected abstract void Autorize(Func<string> getLogin, Func<string> getPassword);
+
+	/// <summary>
+	/// Возвращает данные трека для добавления в очередь воспроизведения
+	/// </summary>
+	/// <param name="tracks">Треки которые уже присутствуют в очереди</param>
+	/// <returns></returns>
+	protected abstract ITrackData GetNextTrack(ITrackData[] tracks);
 
 	#region IUseMenu
 	private MenuItem? _menuItem;
@@ -305,8 +232,8 @@ internal class MusicSystem : IUseMenu
 				{ Keys.Up, () => { if (Volume < 1m) Volume += 0.1m; } },
 				{ Keys.Down, () => { if (Volume > 0m) Volume -= 0.1m; } },
 				{ Keys.Clear, () => SwitchTrackPause() },
-				{ Keys.Left, () => PlayPrevStationTrack() },
-				{ Keys.Right, () => PlayNextStationTrack() },
+				{ Keys.Left, () => PlayPrevTrack() },
+				{ Keys.Right, () => PlayNexTrack() },
 			};
 
 			_menuItem = new(SYSNAME, $"{PATH}/headphones.png", actions);
